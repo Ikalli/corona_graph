423b4f46e1e5345eb209254f4e518cb75e9d31ca
ekalii <andoune0719@gmail.com>
2020-02-04 15:20:41 +0900

    Add Graph

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
+import KoreaGraph from './components/koreagraph';
+import Situation from './components/situation';
+import WorldGraph from './components/worldgraph';
 
-function App() {
-  return (
-    <div className="App">
-      HelloWorld
-    </div>
-  );
+export default class App extends Component{
+  state = {
+    graph: 'korea',
+  }
+
+  handleKoreaClick = () => {
+    this.setState({
+      graph: 'korea'
+    });
+  }
+
+  handleWorldClick = () => {
+    this.setState({
+      graph: 'world'
+    });
+  }
+
+  render(){
+    return(
+      <div className='App'>
+        <h1>신종 코로나 바이러스 그래프</h1>
+        {this.state.graph === 'korea' ? <KoreaGraph /> : <WorldGraph />}
+        <button onClick={this.handleKoreaClick}>국내 정보</button>
+        <button onClick={this.handleWorldClick}>국외 정보</button>
+        <Situation />
+      </div>
+    );
+  }
 }
-
-export default App;
---
+++ src/components/worldgraph.js
@@ -0,0 +1,75 @@
+import React, { Component } from 'react';
+import { LineChart, XAxis, YAxis, CartesianGrid, Line, Tooltip, Legend, ResponsiveContainer} from 'recharts';
+
+const data = [
+	{
+		date: '01.19', 사망자: 4, 확진자: 198,
+	},
+	{
+		date: '01.20', 사망자: 6, 확진자: 270,
+	},
+	{
+		date: '01.21', 사망자: 6, 확진자: 314,
+	},
+	{
+		date: '01.22', 사망자: 17, 확진자: 580,
+	},
+	{
+		date: '01.23', 사망자: 25, 확진자: 830,
+	},
+	{
+		date: '01.24', 사망자: 25, 확진자: 845,
+	},
+	{
+		date: '01.25', 사망자: 41, 확진자: 1315,
+	},
+	{
+		date: '01.26', 사망자: 56, 확진자: 2010,
+	},
+	{
+		date: '01.27', 사망자: 80, 확진자: 2794,
+	},
+	{
+		date: '01.28', 사망자: 106, 확진자: 4572,
+	},
+	{
+		date: '01.29', 사망자: 132, 확진자: 6052,
+	},
+	{
+		date: '01.30', 사망자: 170, 확진자: 7808,
+	},
+	{
+		date: '01.31', 사망자: 213, 확진자: 9805,
+	},
+	{
+		date: '02.01', 사망자: 259, 확진자: 11924,
+	},
+	{
+		date: '02.02', 사망자: 304, 확진자: 14380,
+	},
+	{
+		date: '02.03', 사망자: 362, 확진자: 17363,
+	},
+	{
+		date: '02.04', 사망자: 426, 확진자: 20603,
+	}
+]
+
+export default class WorldGraph extends Component{
+	render(){
+		return(
+			<div>
+				<ResponsiveContainer width='100%' aspect={21.0/9.0}>
+					<LineChart data={data}>
+						<CartesianGrid />
+						<XAxis dataKey='date' interval='preserveEnd' />
+						<YAxis interval='preserveEnd' />
+						<Legend />
+						<Line type='monotone' dataKey="사망자" stroke='red' />
+						<Line type='monotone' dataKey="확진자" stroke='#8884d8' />
+					</LineChart>
+				</ResponsiveContainer>
+			</div>
+		);
+	}
+}
