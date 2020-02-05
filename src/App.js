f48a3668ee783a1edf9c9a8ae22ac715df2dee1d
ekalii <andoune0719@gmail.com>
2020-02-05 10:33:23 +0900

    add source paragraph and css

--- src/App.js
+++ src/App.js
@@ -25,11 +25,12 @@ export default class App extends Component{
     return(
       <div className='App'>
         <h1>신종 코로나 바이러스 현황 그래프</h1>
-        <p>2020.02.04 09:00</p>
+        <p>업데이트: 2020.02.05 09:00</p>
         {this.state.graph === 'korea' ? <KoreaGraph /> : <WorldGraph />}
         <button onClick={this.handleKoreaClick}>국내 정보</button>
         <button onClick={this.handleWorldClick}>국외 정보</button>
         <Situation />
+        <p className='source'>출처: 질병관리본부</p>
       </div>
     );
   }
