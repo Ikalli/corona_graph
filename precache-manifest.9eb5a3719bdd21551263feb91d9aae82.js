7e5f695633511cdb24129f92263ad8f5cf96a3c5
ekalii <andoune0719@gmail.com>
2020-02-05 13:49:47 +0900

    Updates

---
+++ precache-manifest.9eb5a3719bdd21551263feb91d9aae82.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa9a5e991248567b78b5bbce958825dd",
+    "url": "/corona_graph/index.html"
+  },
+  {
+    "revision": "6b158f8c0e4bdec9394f",
+    "url": "/corona_graph/static/css/main.44be392c.chunk.css"
+  },
+  {
+    "revision": "4d29bb11dea8db88f7f7",
+    "url": "/corona_graph/static/js/2.0c658a62.chunk.js"
+  },
+  {
+    "revision": "977b85ae92e0fb68aebdece70c5a2518",
+    "url": "/corona_graph/static/js/2.0c658a62.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b158f8c0e4bdec9394f",
+    "url": "/corona_graph/static/js/main.41d9ca49.chunk.js"
+  },
+  {
+    "revision": "f2b3ef8682d0859bf461",
+    "url": "/corona_graph/static/js/runtime-main.4cfd0e3b.js"
+  }
+]);
