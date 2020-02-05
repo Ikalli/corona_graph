161baf2db895e6e0f16693294c972f32de2ef303
ekalii <andoune0719@gmail.com>
2020-02-05 13:28:48 +0900

    button styling

--- src/App.js
+++ src/App.js
@@ -1,36 +1,45 @@
 import React, { Component } from 'react';
 import './App.css';
 import KoreaGraph from './components/koreagraph';
-import Situation from './components/situation';
+import KoreaSituation from './components/koreasituation';
 import WorldGraph from './components/worldgraph';
+import WorldSituation from './components/worldsituation';
 
 export default class App extends Component{
   state = {
-    graph: 'korea',
+    viewpoint: 'korea',
   }
 
   handleKoreaClick = () => {
     this.setState({
-      graph: 'korea'
+      viewpoint: 'korea'
     });
   }
 
   handleWorldClick = () => {
     this.setState({
-      graph: 'world'
+      viewpoint: 'world'
     });
   }
 
   render(){
     return(
       <div className='App'>
-        <h1>신종 코로나 바이러스 현황 그래프</h1>
+        <div className='headline'>
+          <span className="title">신종 코로나바이러스<br/>현황 그래프</span>
+        </div>
         <p>업데이트: 2020.02.05 09:00</p>
-        {this.state.graph === 'korea' ? <KoreaGraph /> : <WorldGraph />}
-        <button onClick={this.handleKoreaClick}>국내 정보</button>
-        <button onClick={this.handleWorldClick}>국외 정보</button>
-        <Situation />
-        <p className='source'>출처: 질병관리본부</p>
+        {this.state.viewpoint === 'korea' ? <KoreaGraph /> : <WorldGraph />}
+        <div className='btn_group'>
+          <button onClick={this.handleKoreaClick} className='korea'>국내 정보</button>
+          <button onClick={this.handleWorldClick} className='world'>국외 정보</button>
+        </div>
+        {this.state.viewpoint === 'korea' ? <KoreaSituation /> : <WorldSituation />}
+        <div className='tail'>
+          <p className='source'>출처: 질병관리본부, 해외감염병 NOW</p>
+          <p className='feedback'>FeedBack: dev.ekalii@gmail.com</p>
+          <a href="https://github.com/Ekalii/corona_graph" className='github'>Github Source</a>
+        </div>
       </div>
     );
   }
